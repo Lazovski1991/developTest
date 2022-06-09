20d17f1950ec9db8ea4d834bfd6a4441746e8a2e
alazovskiy <alexandr.lazovskiy@shtormtech.ru>
2022-06-10 02:28:23 +0300

    auth

--- src/main/java/my/company/front/config/security/KeycloakSecurityConfig.java
+++ src/main/java/my/company/front/config/security/KeycloakSecurityConfig.java
@@ -7,6 +7,7 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,8 +25,11 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.authorizeRequests()
-                .antMatchers("/developer/*").hasRole("admin")
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE,"/developer/*").hasRole("admin")
+                .antMatchers(HttpMethod.GET,"/developer/*").hasAnyRole("user", "admin")
                 .anyRequest()
                 .permitAll();
         http.csrf().disable();
